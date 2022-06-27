41773b11d4d1ee1cb33208754c63b506a73ea4ad
CARLOS MARTINEZ <carlosmartinez@MacBook-Pro-de-Carlos.local>
2022-06-27 18:01:21 -0400

    step 3 for publish vehicle

--- src/pages/publish-vehicle/photos/index.jsx
+++ src/pages/publish-vehicle/photos/index.jsx
@@ -3,7 +3,17 @@ import './styles.css'
 
 const Photos = () => {
     return (
-        <div>Photos</div>
+        <div className='content-uploads'>
+            <div className='upload-image'>
+                <img src={`/assets/publish-vehicle/upload1.svg`} alt="" />
+            </div>
+            <div className='upload-image'>
+                <img src={`/assets/publish-vehicle/upload2.svg`} alt="" />
+            </div>
+            <div className='upload-image'>
+                <img src={`/assets/publish-vehicle/upload3.svg`} alt="" />
+            </div>
+        </div>
     )
 }
 
